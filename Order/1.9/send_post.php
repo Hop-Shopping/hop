e044464d73322787a506a01ad091dd1a20bbf047
Patrick Mitchell <patrick.jr.mitchell@gmail.com>
2018-07-17 10:03:53 +0930

    removed old order

--- Order/1.9/send_post.php
+++
@@ -1,7 +0,0 @@
-<?php
-//Connecting to sql db.
-$connect = mysqli_connect("https://patrmitch.000webhostapp.com/","id3460448_admin","Pjrjm090402","id3460448_orders");
-//Sending form data to sql db.
-mysqli_query($connect,"INSERT INTO posts (category, title, contents, tags)
-VALUES ('$_POST[post_category]', '$_POST[post_title]', '$_POST[post_contents]', '$_POST[post_tags]')";
-?>
